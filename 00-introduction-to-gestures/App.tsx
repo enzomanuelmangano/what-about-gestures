e46a70361dce3b51513f777e485d8af8f7dd98b7
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2022-01-28 10:17:04 +0100

    chore: initial commit

---
+++ 00-introduction-to-gestures/App.tsx
@@ -0,0 +1,110 @@
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+
+interface AnimatedPosition {
+  x: Animated.SharedValue<number>;
+  y: Animated.SharedValue<number>;
+}
+
+const useFollowAnimatedPosition = ({ x, y }: AnimatedPosition) => {
+  const followX = useDerivedValue(() => {
+    return withSpring(x.value);
+  });
+
+  const followY = useDerivedValue(() => {
+    return withSpring(y.value);
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: followX.value }, { translateY: followY.value }],
+    };
+  });
+
+  return { followX, followY, rStyle };
+};
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
+const SIZE = 80;
+export default function App() {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const context = useSharedValue({ x: 0, y: 0 });
+  // useAnimatedGestureHandler({})
+  const gesture = Gesture.Pan()
+    .onStart(() => {
+      context.value = { x: translateX.value, y: translateY.value };
+    })
+    .onUpdate((event) => {
+      translateX.value = event.translationX + context.value.x;
+      translateY.value = event.translationY + context.value.y;
+    })
+    .onEnd(() => {
+      if (translateX.value > SCREEN_WIDTH / 2) {
+        translateX.value = SCREEN_WIDTH - SIZE;
+      } else {
+        translateX.value = 0;
+      }
+    });
+
+  const {
+    followX: blueFollowX,
+    followY: blueFollowY,
+    rStyle: rBlueCircleStyle,
+  } = useFollowAnimatedPosition({
+    x: translateX,
+    y: translateY,
+  });
+
+  const {
+    followX: redFollowX,
+    followY: redFollowY,
+    rStyle: rRedCircleStyle,
+  } = useFollowAnimatedPosition({
+    x: blueFollowX,
+    y: blueFollowY,
+  });
+
+  const { rStyle: rGreenCircleStyle } = useFollowAnimatedPosition({
+    x: redFollowX,
+    y: redFollowY,
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[styles.circle, { backgroundColor: 'green' }, rGreenCircleStyle]}
+      />
+      <Animated.View
+        style={[styles.circle, { backgroundColor: 'red' }, rRedCircleStyle]}
+      />
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={[styles.circle, rBlueCircleStyle]} />
+      </GestureDetector>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  circle: {
+    position: 'absolute',
+    height: SIZE,
+    aspectRatio: 1,
+    backgroundColor: 'blue',
+    borderRadius: SIZE / 2,
+    opacity: 0.8,
+  },
+});
---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center">
+What about Gestures? 👌🏼
+</h1>
+
+ <a href="https://youtu.be/xpT2shjX790" target="_blank">
+      <img src="https://raw.githubusercontent.com/enzomanuelmangano/what-about-gestures/main/.assets/what-about-gestures.png" title="what-about-gestures">
+  </a>
+
+What about Gestures is a Youtube Playlist where I like to share the process of creating React Native animations. All animations are always made from scratch trying to make them easy to understand.
+
+The basis of all animations are always the following packages:
+
+- [react-native-gesture-handler](https://docs.swmansion.com/react-native-gesture-handler/docs)
+- [react-native-reanimated v2](https://docs.swmansion.com/react-native-reanimated/docs/)
+
+Sounds interesting? [Click here!](https://youtu.be/xpT2shjX790)
+
+## Available Tutorials
+
+- [00 Introduction to Gesture Handler v2](https://youtu.be/xpT2shjX790)
